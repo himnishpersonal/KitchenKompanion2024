a4dfa25665efb764111ae206d461b1304cef075f
himnish03 <123763115+himnish03@users.noreply.github.com>
2024-09-17 22:20:32 -0400

    intial code

---
+++ app.js
@@ -0,0 +1,7 @@
+function openTab(tabNumber) {
+    if (tabNumber === 1) {
+        document.getElementById('tab_one').style.display = 'block'; 
+    }else if(tabNumber === 2){
+        document.getElementById('tab_two').style.display = 'block'; 
+    }
+}
