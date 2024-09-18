6a967413d323e67d9cc5c43dd47b30b631c77494
himnish03 <123763115+himnish03@users.noreply.github.com>
2024-09-18 13:27:18 -0400

    fixed tab bar logic and scrolling/dimensions for tab 1

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 function openTab(tabNumber) {
     if (tabNumber === 1) {
         document.getElementById('tab_one').style.display = 'block'; 
+        document.getElementById('tab_two').style.display = 'none'; 
+        document.getElementById('tab_three').style.display = 'none'; 
+        document.getElementById('tab_four').style.display = 'none'; 
+        document.getElementById('tab_five').style.display = 'none'; 
     }else if(tabNumber === 2){
+        document.getElementById('tab_one').style.display = 'none'; 
         document.getElementById('tab_two').style.display = 'block'; 
+        document.getElementById('tab_three').style.display = 'none'; 
+        document.getElementById('tab_four').style.display = 'none'; 
+        document.getElementById('tab_five').style.display = 'none'; 
+    }else if(tabNumber === 3){
+        document.getElementById('tab_one').style.display = 'none'; 
+        document.getElementById('tab_two').style.display = 'none'; 
+        document.getElementById('tab_three').style.display = 'block'; 
+        document.getElementById('tab_four').style.display = 'none'; 
+        document.getElementById('tab_five').style.display = 'none'; 
+    }else if(tabNumber === 4){
+        document.getElementById('tab_one').style.display = 'none'; 
+        document.getElementById('tab_two').style.display = 'none'; 
+        document.getElementById('tab_three').style.display = 'none'; 
+        document.getElementById('tab_four').style.display = 'block'; 
+        document.getElementById('tab_five').style.display = 'none'; 
+    }else if(tabNumber === 5){
+        document.getElementById('tab_one').style.display = 'none'; 
+        document.getElementById('tab_two').style.display = 'none'; 
+        document.getElementById('tab_three').style.display = 'none'; 
+        document.getElementById('tab_four').style.display = 'none'; 
+        document.getElementById('tab_five').style.display = 'block'; 
     }
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    openTab(1); 
+});
